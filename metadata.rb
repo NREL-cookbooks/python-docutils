65238c77e9729cdce238004ffee73ecdd2251121
Nick Muerdter <nick.muerdter@nrel.gov>
2014-07-13 21:49:32 -0600

    First commit to install python-docutils via chef.

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'python-docutils'
+maintainer       'National Renewable Energy Laboratory'
+maintainer_email 'nick.muerdter@nrel.gov'
+license          'All rights reserved'
+description      'Installs/Configures python-docutils'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'python'
---
+++ recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: python-docutils
+# Recipe:: default
+#
+# Copyright 2014, National Renewable Energy Laboratory
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "python"
+
+package "python-docutils"
